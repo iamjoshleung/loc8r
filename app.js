d2a298b65f4486e229890630272c64478db65d09
joshuajazleung <joshuajazleung@gmail.com>
2016-04-30 19:45:06 -0700

    All files

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+var uglifyJs = require('uglify-js');
+var fs = require('fs');
+var passport = require('passport');
 
-var routes = require('./app_server/routes/index');
+require('./app_api/models/db');
+require('./app_api/config/passport');
+
+// var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 var users = require('./app_server/routes/users');
 
 var app = express();
@@ -15,6 +22,36 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
+var appClientFiles = [
+  'app_client/app.js',
+  'app_client/home/home.controller.js',
+  'app_client/about/about.controller.js',
+  'app_client/locationDetail/locationDetail.controller.js',
+  'app_client/reviewModal/reviewModal.controller.js',
+  'app_client/auth/register/register.controller.js',
+  'app_client/auth/login/login.controller.js',
+  'app_client/common/services/geolocation.service.js',
+  'app_client/common/services/loc8rData.service.js',
+  'app_client/common/services/authentication.service.js',
+  'app_client/common/filters/formatDistance.filter.js',
+  'app_client/common/filters/addHtmlLineBreaks.filter.js',
+  'app_client/common/directive/ratingStars/ratingStars.directive.js',
+  'app_client/common/directive/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directive/navigation/navigation.directive.js',
+  'app_client/common/directive/navigation/navigation.controller.js',
+  'app_client/common/directive/pageHeader/pageHeader.directive.js'
+];
+
+var uglified = uglifyJs.minify(appClientFiles, { compress: false });
+
+fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Script generated and saved: loc8r.min.js');
+  }
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,9 +59,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+app.use(passport.initialize());
+
+// app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/users', users);
+
+app.use(function (req, res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,6 +80,13 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
+// Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message": err.name + ": " + err.message});
+  }
+});
 
 // development error handler
 // will print stacktrace
---
+++ app_client/reviewModal/reviewModal.controller.js
@@ -0,0 +1,44 @@
+(function () {
+  angular
+    .module('loc8rApp')
+    .controller('reviewModalCtrl', reviewModalCtrl);
+
+  reviewModalCtrl.$inject = ['$uibModalInstance', 'locationData', 'loc8rData'];
+  function reviewModalCtrl ($uibModalInstance, locationData, loc8rData) {
+    var vm = this;
+    vm.locationData = locationData;
+    vm.formData = {};
+
+    vm.doAddReview = function (locationid, formData) {
+      loc8rData.addReviewById(locationid, {
+        rating: formData.rating,
+        reviewText: formData.reviewText
+      })
+        .success(function (data) {
+          vm.modal.close(data);
+        })
+        .error(function (data) {
+          vm.formError = "Your review has not been saved, try again.";
+        });
+    };
+
+    vm.modal = {
+      close: function (result) {
+        $uibModalInstance.close(result);
+      },
+      cancel: function () {
+        $uibModalInstance.dismiss('cancel');
+      }
+    };
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if(!vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+        vm.doAddReview(vm.locationData.locationid, vm.formData);
+      }
+    };
+  }
+})();
